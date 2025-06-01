164d5bd578c131d4d564ae7a553306d969e60c08
DragonWarrior💫 <supremedragonwarrior@gmail.com>
2025-06-01 19:16:51 -0300

    bingo grid responsive design

--- app/taxo-bingo/components/BingoGrid.tsx
+++ app/taxo-bingo/components/BingoGrid.tsx
@@ -21,7 +21,7 @@ export default function BingoGrid({
 }: Props) {
   return (
     <div
-      className={`grid grid-cols-4 gap-4 ${
+      className={`grid grid-cols-3 md:grid-cols-4 gap-4 ${
         disabled ? "opacity-50 pointer-events-none" : ""
       }`}
     >
@@ -31,9 +31,10 @@ export default function BingoGrid({
         return (
           <div
             key={i}
-            className={`border-2 border-solid border-[rgb(47,37,81)] w-40 h-28 bg-custom-3 text-white rounded flex items-center justify-center text-center font-bold p-2 cursor-pointer transition duration-300 hover:opacity-80 ${
-              filled ? "pointer-events-none opacity-70" : ""
-            } ${isShaking ? "shake-wrong" : ""}`}
+            className={`border-2 border-solid border-[rgb(29,35,73)] md:w-40 h-28 bg-custom-6 text-xs md:text-base text-white rounded flex items-center 
+              justify-center text-center font-bold p-2 cursor-pointer transition duration-300 hover:opacity-80 ${
+                filled ? "pointer-events-none opacity-70" : ""
+              } ${isShaking ? "shake-wrong" : ""}`}
             onClick={() => onCellClick(category)}
           >
             {filled ? (
