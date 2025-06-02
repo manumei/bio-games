e68626ebf7ed66c4c80ba74b2adfb3ac2e5ca32f
Manuel Meiriño <mmeirino@udesa.edu.ar>
2025-06-02 12:00:28 -0300

    cell instead of linxicon at menu, media query for subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Panel from "./components/Panel";
 export default function Home() {
   return (
     <main className="min-h-[80vh] bg-custom-2 flex flex-col">
-      <h2 className="mt-8 text-xl font-thin text-center">
+      <h2 className="mt-8 text-base sm:text-xl font-thin text-center">
         Select the biology game you want to play:
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-8 p-5 mx-auto w-fit">
@@ -17,7 +17,7 @@ export default function Home() {
         <Panel title="Symptoms Link" coming_soon />
         <Panel title="Animal Tic-Tac-Toe" coming_soon />
         <Panel title="Bio Top 8" coming_soon />
-        <Panel title="Bio-Linxicon" coming_soon />
+        <Panel title="Cell Classifier" coming_soon />
       </div>
     </main>
   );
